16abbf41e16b5eeafddb688411aeaf136a3030c9
Henry <henryb2533@gmail.com>
2019-08-29 11:41:54 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ *Authors: Henry Buscher Diego Almanza & Isaac Baysinger
+ *Date: 2019/29/08
+ *
+ *Prints names of Authors
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+	prinf("Henry Buscher, Diego Almanza & Isaac Baysinger/n");
+
+	return 0;
+}
