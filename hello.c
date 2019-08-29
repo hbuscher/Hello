766658feb08813ed0976cac6ed0cb709e807f8ca
Henry <henryb2533@gmail.com>
2019-08-29 12:03:12 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,16 +2,16 @@
  *Authors: Henry Buscher & Diego Almanza
  *Date: 2019/29/08
  *
- *Prints names of Authors, and Majors
+ *Prints names of Author, and Major
  */
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(int argc, char **argv) {
 
-	printf("Henry Buscher & Diego Almanza/n");
-//added majors of authors
-	printf("Computer Engineering Majors"/n);
+	printf("Henry Buscher/n");
+//added major of author
+	printf("Computer Engineering"/n);
 
 	return 0;
 }
